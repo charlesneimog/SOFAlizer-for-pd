08f5c4c0a8a05949411e995bff9a86baf7be6552
Charles K. Neimog <charlesneimog@outlook.com>
2023-10-09 19:20:04 -0300

    add python script to handle dep

--- resources/localdeps/localdeps.py
+++ resources/localdeps/localdeps.py
@@ -6,7 +6,7 @@ import os
 import re
 
 linuxLibraries = ["libm.so.6", "linux-vdso.so.1", "libc.so.6", "ld-linux-x86-64.so.2"]
-
+windowsLibraries = ["ntdll.dll", "KERNEL32.DLL", "KERNELBASE.dll", "msvcrt.dll", "pd.dll", "KERNEL32.DLL"]
 class localdeps:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='Local dependencies')
@@ -15,6 +15,8 @@ class localdeps:
         self.args = self.parser.parse_args()
         if ".pd_linux" in self.args.pdobj or ".l_amd64" in self.args.pdobj:
             self.parse_linux()
+        elif ".dll" in self.args.pdobj or ".m_amd64" in self.args.pdobj:
+            self.parse_windows()
 
     def parse_linux(self):
         # check if ldd and patchelf are installed
@@ -48,6 +50,8 @@ class localdeps:
                                     for file in files:
                                         if file == libraryName:
                                             libraryFound = True
+                                            if not os.path.exists("amd64"):
+                                                os.mkdir("amd64")
                                             if not os.path.exists("amd64/" + libraryName):
                                                 shutil.copy(os.path.join(root, file), "./amd64/" + libraryName)
                         if not libraryFound:
@@ -65,7 +69,48 @@ class localdeps:
                     if not os.path.exists("amd64/" + libraryName):
                         shutil.copy(library, "amd64/" + libraryName)
                     os.system("patchelf --set-rpath \\$ORIGIN/amd64/" + f" {self.args.pdobj}")
-
+                    
+    def parse_windows(self):
+        command = []
+        command.append("ntldd")
+        command.append(self.args.pdobj)
+        ldd = subprocess.run(command, capture_output=True)
+        for library_info in ldd.stdout.decode("utf-8").split("\n"):
+            systemLibrary = False
+            for sysLib in windowsLibraries:
+                dynamiclibraryName = os.path.basename(library_info).lower()
+                if sysLib.lower() in dynamiclibraryName or sysLib == self.args.pdobj:
+                    systemLibrary = True
+                    break   
+            if systemLibrary:
+                continue
+            if "not found" in library_info:
+                if self.args.includes:
+                    pattern = r'(\S+)\s=>'
+                    match = re.search(pattern, library_info)
+                    libraryFound = False
+                    if match:
+                        libraryName = match.group(1)
+                        print("Searching for " + libraryName)
+                        for include in self.args.includes.split(" "):
+                            if os.path.exists(include):
+                                for root, _, files in os.walk(include):
+                                    for file in files:
+                                        if file == libraryName:
+                                            libraryFound = True
+                                            if not os.path.exists(libraryName):
+                                                shutil.copy(os.path.join(root, file), libraryName)
+                        if not libraryFound:
+                            print("Library not found: " + libraryName)
+                            sys.exit(1)
+            else:
+                pattern = r'=>\s(.*?)\s'
+                match = re.search(pattern, library_info)
+                if match:
+                    library = match.group(1)
+                    libraryName = os.path.basename(library)
+                    if not os.path.exists(libraryName):
+                        shutil.copy(library, libraryName)
 
                     
 
